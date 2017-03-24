54649407876938879847c1fda3f3930b35bd8330
David Juckes <david.a.juckes@tesco.net>
2017-03-24 18:57:34 +0000

    Flags can now set username

--- launcher.py
+++ launcher.py
@@ -1,4 +1,4 @@
-global flags, theme_name_entry
+global flags, theme_name_entry, username_entry
 import tkinter as tk
 import os, threading
 
@@ -15,6 +15,7 @@ root.title('Messaging Launcher')
 
 def launch():
     flags['theme'] = theme_name_entry.get()
+    flags['username'] = username_entry.get()
     file = open('flags.py', 'w')
     file.write(str(flags))
     file.close()
@@ -26,10 +27,20 @@ go_button = tk.Button(root, text='Launch', command=launch, font=('', 15))
 
 input_frame = tk.Frame(root)
 
+####
+
 label_frame = tk.Frame(input_frame)
+
+username_label = tk.Label(label_frame, text='Name')
 theme_name_label = tk.Label(label_frame, text='Theme')
 
+#####
+
 entry_frame = tk.Frame(input_frame)
+
+username_entry = tk.Entry(entry_frame)
+username_entry.insert(0, flags['username'])
+
 theme_name_entry = tk.Entry(entry_frame)
 theme_name_entry.insert(0, flags['theme'])
 
@@ -37,6 +48,9 @@ theme_name_entry.insert(0, flags['theme'])
 
 label_frame.pack(side=tk.LEFT)
 entry_frame.pack(side=tk.LEFT)
+
+username_label.pack(anchor='nw')
+username_entry.pack(anchor='nw')
 
 theme_name_label.pack(anchor='nw')
 theme_name_entry.pack(anchor='nw')
